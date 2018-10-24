1191e00ff51b0ccf557cf6477ca591217a940c82
Sergeim <smiroshnikov@securitydam.com>
2018-10-24 19:06:43 +0300

    interfaces part 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        //DeskPhone sergeiMPhone;
 
+        //DeskPhone sergeiMPhone; // this is a valid instantiation as well
         // interface usage
-        
+
         ITelephone sergeiMPhone;
         sergeiMPhone = new DeskPhone(54686445);
 
         sergeiMPhone.powerOn();
-        sergeiMPhone.callPhone(54686445);
+        boolean flag = sergeiMPhone.callPhone(54686445);
+        System.out.println("the phone ring status is :" + flag);
+
         sergeiMPhone.answer();
 
+        //the line below is POSSIBLE only because they share same interface
+        sergeiMPhone = new MobilePhone(234456);
+        sergeiMPhone.powerOn();
+        sergeiMPhone.callPhone(234456);
 
     }
 }
---
+++ src/MobilePhone.java
@@ -0,0 +1,51 @@
+public class MobilePhone implements ITelephone {
+
+    private int myNumber;
+    private boolean isRinging;
+    private boolean isOn;
+
+    MobilePhone(int myNumber) {
+        this.myNumber = myNumber;
+    }
+
+    @Override
+    public void powerOn() {
+        isOn = true;
+        System.out.println("Phone is powered up ... ");
+
+    }
+
+    @Override
+    public void dial(int phoneNumber) {
+        if (isOn) {
+            System.out.println("Now ringing " + phoneNumber + "on a mobile phone");
+        } else {
+            System.out.println("Phone is OFF !");
+        }
+
+    }
+
+    @Override
+    public boolean callPhone(int phoneNumber) {
+        if (phoneNumber == myNumber && isOn) {
+            isRinging = true;
+            System.out.println("Melody Playing .....!");
+        } else {
+            isRinging = false;
+            System.out.println("Mobile Phone turned off or number is different");
+        }
+        return isRinging;
+
+    }
+
+    @Override
+    public boolean isRinging() {
+        return isRinging;
+    }
+
+    @Override
+    public void answer() {
+        System.out.println("Answering mobile phone");
+        isRinging = false;
+    }
+}
