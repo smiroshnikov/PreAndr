085e8d1d95a1ba00432bb21494e85b45147e2546
Sergeim <smiroshnikov@securitydam.com>
2018-10-30 14:49:45 +0200

    solving challenge 1

---
+++ src/solution_4_HW/Resume.java
@@ -0,0 +1,15 @@
+package solution_4_HW;
+
+/**
+ * Initial resume class
+ */
+public class Resume {
+
+    // Unique identifier
+    String uuid;
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+}
